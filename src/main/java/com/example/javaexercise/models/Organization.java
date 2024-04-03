47bd29f764f53f0cf26853cac0d81463ead1379c
BelovUser <139882857+BelovUser@users.noreply.github.com>
2024-04-03 15:52:38 +0200

    Created Entity Organization class with relationships: One to Many Employee class

---
+++ src/main/java/com/example/javaexercise/models/Organization.java
@@ -0,0 +1,49 @@
+package com.example.javaexercise.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
+public class Organization {
+    @Id
+    private Long id;
+    private String name;
+    private String adress;
+    @OneToMany(mappedBy = "organization")
+    private List<Employee> employee;
+
+    public List<Employee> getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(List<Employee> employee) {
+        this.employee = employee;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String surname) {
+        this.adress = surname;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
