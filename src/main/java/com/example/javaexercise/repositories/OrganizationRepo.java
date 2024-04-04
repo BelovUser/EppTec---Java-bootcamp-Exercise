00f900ae74c5137b8f498271e02e786b1bfece16
BelovUser <139882857+BelovUser@users.noreply.github.com>
2024-04-04 08:44:30 +0200

    Services: Added findAllByName query int OrganizationRepo

--- src/main/java/com/example/javaexercise/repositories/OrganizationRepo.java
+++ src/main/java/com/example/javaexercise/repositories/OrganizationRepo.java
@@ -1,7 +1,11 @@
 package com.example.javaexercise.repositories;
 
-import com.example.javaexercise.models.Employee;
+import com.example.javaexercise.models.Organization;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrganizationRepo extends CrudRepository<Employee, Long> {
+import java.util.Optional;
+
+public interface OrganizationRepo extends CrudRepository<Organization, Long> {
+
+    Optional<Organization> findAllByName(String name);
 }
