6c2398ad75ae52ac7f8f6072d13432fa8533eb3b
BelovUser <139882857+BelovUser@users.noreply.github.com>
2024-04-04 08:43:06 +0200

    Services: Created EmployeeService with simple crud methods

---
+++ src/main/java/com/example/javaexercise/services/EmployeeService.java
@@ -0,0 +1,63 @@
+package com.example.javaexercise.services;
+
+import com.example.javaexercise.dtos.EmployeeDTO;
+import com.example.javaexercise.models.Employee;
+import com.example.javaexercise.models.Organization;
+import com.example.javaexercise.repositories.EmployeeRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+    private final OrganizationService organizationService;
+
+    public EmployeeService(EmployeeRepo employeeRepo, OrganizationService organizationService) {
+        this.employeeRepo = employeeRepo;
+        this.organizationService = organizationService;
+    }
+
+   public Optional<Employee> findById(Long employeeId){
+        return employeeRepo.findById(employeeId);
+   }
+
+   public void createEmployee(EmployeeDTO employeeDTO){
+        Employee newEmployee = new Employee();
+        newEmployee.setName(employeeDTO.name());
+        newEmployee.setUsername(employeeDTO.username());
+        newEmployee.setBirthday(employeeDTO.birthday());
+
+        employeeRepo.save(newEmployee);
+   }
+
+   public void deleteEmployee(Long employeeId){
+        employeeRepo.deleteById(employeeId);
+   }
+
+   public void setSupEmployee(Long supEmployeeId, Long suBEmployeeId){
+        Employee supEmployee = findById(supEmployeeId).get();
+        Employee subEmployee = findById(suBEmployeeId).get();
+
+        supEmployee.getSubEmployees().add(subEmployee);
+        subEmployee.setSupEmployee(supEmployee);
+   }
+
+   public boolean assigneEmployeeToOrganization(Long employeeId, String organizationName){
+       Optional<Employee> optEmployee = findById(employeeId);
+       Optional<Organization> optOrganization = organizationService.findByName(organizationName);
+       if(optEmployee.isEmpty() || optOrganization.isEmpty()){
+           return false;
+       }
+       Employee employee = optEmployee.get();
+       Organization organization = optOrganization.get();
+
+       organization.getEmployee().add(employee);
+       organizationService.save(organization);
+
+       employee.setOrganization(organization);
+       employeeRepo.save(employee);
+
+       return true;
+   }
+}
