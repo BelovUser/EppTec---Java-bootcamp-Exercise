3fa3b872008d33815c7ce217347923978590b17c
BelovUser <139882857+BelovUser@users.noreply.github.com>
2024-04-08 17:37:04 +0200

    Testing: Created test class for DtoMapper with test cases.

---
+++ src/test/java/com/example/javaexercise/mappers/DtoMapperTest.java
@@ -0,0 +1,86 @@
+package com.example.javaexercise.mappers;
+
+import com.example.javaexercise.dtos.CreateEmployeeDto;
+import com.example.javaexercise.dtos.CreateOrganizationDto;
+import com.example.javaexercise.dtos.EmployeeDto;
+import com.example.javaexercise.dtos.OrganizationDto;
+import com.example.javaexercise.models.Employee;
+import com.example.javaexercise.models.Organization;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class DtoMapperTest {
+
+    @Autowired
+    private DtoMapper dtoMapper;
+    @Test
+    void mapToEmployeeDTO_givenEmployee_shouldReturnEmployeeDto() {
+        //arrange
+        Employee employee = new Employee();
+        employee.setId(1L);
+        employee.setName("John");
+        employee.setSurname("Doe");
+        employee.setBirthday(new Date(1999, Calendar.DECEMBER,12));
+
+        EmployeeDto expectedEmployeeDto = new EmployeeDto(1L,"John","Doe",
+                "12-12-1999","none","none", List.of());
+        //act
+        EmployeeDto actualEmployeeDto = dtoMapper.mapToEmployeeDTO(employee);
+        //assert
+        assertEquals(expectedEmployeeDto,actualEmployeeDto);
+    }
+
+    @Test
+    void mapToOrganizationDTO_givenOrganization_shouldReturnOrganizationDto() {
+        //arrange
+        Organization organization = new Organization();
+        organization.setId(1L);
+        organization.setName("Org");
+        organization.setAddress("st.Peter 123");
+
+        OrganizationDto expectedOrganizationDto = new OrganizationDto(1L,"Org","st.Peter 123", List.of());
+        //act
+        OrganizationDto actualOrganizationDto = dtoMapper.mapToOrganizationDTO(organization);
+        //assert
+        assertEquals(expectedOrganizationDto,actualOrganizationDto);
+    }
+
+    @Test
+    void mapDtoToEmployee_givenCreateEmployeeDto_shouldReturnEmployee() {
+        //arrange
+        CreateEmployeeDto createEmployeeDto = new CreateEmployeeDto("John", "Doe", new Date(1999, Calendar.DECEMBER,12));
+
+        Employee expectedEmployee = new Employee();
+        expectedEmployee.setName("John");
+        expectedEmployee.setSurname("Doe");
+        expectedEmployee.setBirthday(new Date(1999, Calendar.DECEMBER,12));
+        //act
+        Employee actualEmployee = dtoMapper.mapDtoToEmployee(createEmployeeDto);
+        //assert
+        assertEquals(expectedEmployee.getName(),actualEmployee.getName());
+        assertEquals(expectedEmployee.getSurname(),actualEmployee.getSurname());
+        assertEquals(expectedEmployee.getBirthday(),actualEmployee.getBirthday());
+    }
+
+    @Test
+    void mapDtoToOrganization_givenOrganization_shouldReturnOrganization() {
+        //arrange
+        CreateOrganizationDto createOrganizationDto = new CreateOrganizationDto("Org", "st.Peter 123");
+
+        Organization expectedOrganization = new Organization();
+        expectedOrganization.setName("Org");
+        expectedOrganization.setAddress("st.Peter 123");
+        //act
+        Organization actualOrganization = dtoMapper.mapDtoToOrganization(createOrganizationDto);
+        //assert
+        assertEquals(expectedOrganization.getName(),actualOrganization.getName());
+        assertEquals(expectedOrganization.getAddress(),actualOrganization.getAddress());
+    }
+}
