327fd06f897de537048bf24373a7fc05b845d1c2
BelovUser <139882857+BelovUser@users.noreply.github.com>
2024-04-11 08:10:24 +0200

    Repositories: Added queries

--- src/main/java/com/example/javaexercise/repositories/EmployeeRepository.java
+++ src/main/java/com/example/javaexercise/repositories/EmployeeRepository.java
@@ -8,5 +8,9 @@ import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+    List<Employee> findAllByNameOrSurnameOrId(String name, String surname, Long id);
+    List<Employee> findAllByNameAndSurnameAndId(String name, String surname, Long id);
+    List<Employee> findAllByNameAndIdOrSurnameAndId(String name, Long id, String surname, Long id2);
     List<Employee> findAllByNameOrSurname(String name, String surname);
+    List<Employee> findAllByNameAndSurname(String name, String surname);
 }
