1057203a9831cfe62336a1612875b63041107938
BelovUser <139882857+BelovUser@users.noreply.github.com>
2024-04-12 14:06:15 +0200

    Exception Handling: Created ApiException as patter for other exceptions

---
+++ src/main/java/com/example/javaexercise/exceptions/ApiException.java
@@ -0,0 +1,36 @@
+package com.example.javaexercise.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiException {
+    private final String message;
+    private final Throwable throwable;
+    private final HttpStatus httpStatus;
+
+    public ApiException(String message, Throwable throwable, HttpStatus httpStatus) {
+        this.message = message;
+        this.throwable = throwable;
+        this.httpStatus = httpStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Throwable getThrowable() {
+        return throwable;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "ApiException{" +
+                "message='" + message + '\'' +
+                ", throwable=" + throwable +
+                ", httpStatus=" + httpStatus +
+                '}';
+    }
+}
