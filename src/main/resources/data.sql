8e08500da2552fe1edde8af875a64d9a86cd4d7c
BelovUser <139882857+BelovUser@users.noreply.github.com>
2024-04-15 09:02:40 +0200

    Refactor: Added seeding data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,14 @@
 INSERT INTO employee (id,name,surname,birthday)
 VALUES (NEXTVAL('employee_seq'),'Adam','Madam','1999-12-12');
+INSERT INTO employee (id,name,surname,birthday)
+VALUES (NEXTVAL('employee_seq'),'Radka','Madam','1988-03-10');
+INSERT INTO employee (id,name,surname,birthday)
+VALUES (NEXTVAL('employee_seq'),'Katerina','Madam','1988-06-03');
+
+INSERT INTO organization (id,name,address)
+VALUES (NEXTVAL('organization_seq'),'eppTec','Anglicka 20');
+INSERT INTO organization (id,name,address)
+VALUES (NEXTVAL('organization_seq'),'Pepco S.R.O','Italska 20');
+
+
+
